16aa9ec0a1af7c3b623583d4f6bd522c83ff4896
kashmary <treesolosapling@gmail.com>
2023-04-30 17:31:47 -0500

    "removed lambdaurl"

--- get-function/main.go
+++ get-function/main.go
@@ -7,7 +7,6 @@ import (
     "github.com/aws/aws-sdk-go/aws"
     "github.com/aws/aws-sdk-go/aws/session"
     "github.com/aws/aws-sdk-go/service/dynamodb"
-    "github.com/aws/aws-lambda-go/lambdaurl"
     
 )
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
